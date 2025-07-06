5c39a744d794595b51e04fb138c4610f1b2f443d
Jus10-CYA <siajustin71@gmail.com>
2025-07-06 23:04:15 +0800

    Mango graham shake

--- stack.c
+++ stack.c
@@ -10,35 +10,54 @@
 #include "stack.h"
 
 /*
-    Initializes an empty stack
+    Purpose: Initializes an empty stack
+    Returns: void
     @param *s Pointer to a stack s
+    Pre-condition:
+    - None
+    Post-condition:
+    - Stack s is initialized
 */
 void CREATE(Stack *s) {
     s->top = -1;
 }
 
 /*
-    Determines if a stack has no elements
+    Purpose: Determines if a stack has no elements
+    Returns: 1 if stack is empty, 0 if not empty
     @param *s Pointer to a stack s
-    @return 1 if stack is empty, else 0
+    Pre-condition:
+    - None
+    Post-condition:
+    - Returns evaluation of emptiness of the stack
 */
 int ISEMPTY(Stack *s) {
     return s->top < 0;
 }
 
 /*
-    Determines if a stack has maximum number of elements
+    Purpose: Determines if a stack has maximum number of elements
+    Returns: 1 if stack is full, 0 if not full
     @param *s Pointer to a stack s
-    @return 1 if stack is full, else 0
+    Pre-condition:
+    - None
+    Post-condition:
+    - Returns evaluation of fullness of the stack
 */
 int ISFULL(Stack *s) {
     return s->top >= MAX - 1;
 }
 
 /*
-    Returns the point at the secondmost top position of the stack
+    Purpose: Checks if there are 2 or more points in the stack 
+             and gives the point at the secondmost top position of the stack
+    Returns: If there are 2 or more points, return secondmost top point in the stack
+             if there are less than 2 points, returns a point with -999 x and y coordinate values
     @param *s Pointer to a stack s
-    @return Secondmost top point in the stack
+    Pre-condition:
+    - None
+    Post-condition:
+    - Returns the secondmost top point in the stack
 */
 Point NEXT_TO_TOP(Stack *s) {
     if (s->top > 0) {//runs if there are more than 2 points in the stack
@@ -53,9 +72,15 @@ Point NEXT_TO_TOP(Stack *s) {
 }
 
 /*
-    Places an element on the topmost element of the stack
+    Purpose: Checks if a stack is full and places an element
+             on the topmost element of the stack
+    Returns: void
     @param *s Pointer to a stack s
     @param elem Point to be inserted in the stack
+    Pre-condition:
+    - None
+    Post-condition:
+    - New point is added to top of the stack
 */
 void PUSH(Stack *s, Point elem) {
     if (!ISFULL(s)) {//runs if stack is not full
@@ -66,9 +91,14 @@ void PUSH(Stack *s, Point elem) {
 }
 
 /*
-    Removes the topmost element of the stack
+    Purpose: Checks if a stack is empty and removes the topmost element of the stack
+    Returns: If there is at least one point in the stack, returns top point
+             if there are less than 2 points, returns a point with -999 x and y coordinate values
     @param *s Pointer to a stack s
-    @return Top point in stack
+    Pre-condition:
+    - None
+    Post-condition:
+    - Top point is removed from the stack and returned
 */
 Point POP(Stack *s) {
     if (!ISEMPTY(s)) {//runs if there is AT LEAST one point in the stack
@@ -83,9 +113,14 @@ Point POP(Stack *s) {
 }
 
 /*
-    Returns the topmost element of the stack
+    Purpose: Checks if a stack is empty and gives the topmost element of the stack
+    Returns: If there is at least one point in the stack, returns top point
+             if there are less than 2 points, returns a point with -999 x and y coordinate values
     @param *s Pointer to a stack s
-    @return Top point in stack
+    Pre-condition:
+    - None
+    Post-condition:
+    - Top point is returned
 */
 Point TOP(Stack *s) {
     if (!ISEMPTY(s)) {//runs if there is AT LEAST one point in the stack
